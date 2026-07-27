c133eb32af7e2cca7b3b2cf1680b3d9629458751
naladar2 <naladar2@gmail.com>
2026-07-27 14:55:28 +0300

    Add files via upload

--- warehouse-pwa-v2/sw.js
+++ warehouse-pwa-v2/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'warehouse-v12';
+const CACHE = 'warehouse-v13';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 const NETWORK_TIMEOUT = 4000;
 
